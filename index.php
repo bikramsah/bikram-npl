23953b490a05136b6692798e7f619c127f0694f1
bikramsah <bikramsah646@gmail.com>
2025-05-05 11:50:26 +0545

    newmodel

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+echo '<h1>Welcome to Nepal !!!!!</h1>'
 echo '<h1>Welcome to Janakpur,Madhesh Province !</h1>';
 echo'<img src="janakpur.jpg"width="1524px">';
 echo '<h1>Welcome to Koshi Province !</h1>';
