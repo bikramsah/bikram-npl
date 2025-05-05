951305d59a497f56e5d83c4509bb03be3ade3148
bikramsah <bikramsah646@gmail.com>
2025-05-05 11:56:36 +0545

    size fixed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo '<h1>Welcome to Nepal...</h1>';
 echo '<h1>Welcome to Janakpur,Madhesh Province !</h1>';
-echo'<img src="janakpur.jpg"width="1524px">';
+echo'<img src="janakpur.jpg"width="1224px">';
 echo '<h1>Welcome to Koshi Province !</h1>';
 echo'<img src="koshi.jpg"width="2024px">';
 ?>
