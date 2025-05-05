cc450d81e8a8a7c406b6218b26cd4d02fab790f7
bikramsah <bikramsah646@gmail.com>
2025-05-05 11:17:39 +0545

    sakigoni

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h1>Welcome to Janakpur,Madhesh Province !</h1>';
+echo'<img src="janakpur.jpg"width="1524px">';
+echo '<h1>Welcome to Koshi Province !</h1>';
+echo'<img src="koshi.jpg"width="2024px">';
+?>
